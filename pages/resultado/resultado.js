3a353d47a5ebfb1ce08f7a88edec3adb09fffafc
rick915 alves <henriquelago2017@gmail.com>
2024-06-09 14:02:05 +0000

    adicionado o javascript no resultado

---
+++ pages/resultado/resultado.js
@@ -0,0 +1,9 @@
+import { trocarTema, verificarTema } from "../../helpers/tema-helper.js"
+
+const botaotema = document.querySelector(".tema button")
+const body = document.querySelector("body")
+botaotema.addEventListener("click", () => {
+    trocarTema(body, botaotema)
+})
+
+verificarTema(body, botaotema)
