dd5898c591c5b3cece4026f195c8dd0242432b8e
rick915 alves <henriquelago2017@gmail.com>
2024-06-09 14:55:10 +0000

    alteraçao dinamica do assunto da pagina

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -2,8 +2,25 @@ import { trocarTema, verificarTema } from "../../helpers/tema-helper.js"
 
 const botaotema = document.querySelector(".tema button")
 const body = document.querySelector("body")
+
 botaotema.addEventListener("click", () => {
     trocarTema(body, botaotema)
 })
 
 verificarTema(body, botaotema)
+
+const assunto = localStorage.getItem("assunto")
+
+
+function alterarAssunto(){
+    const divIcone = document.querySelector(".assunto_icone")
+    const iconeImg = document.querySelector(".assunto_icone img")
+    const assuntoTitulo = document.querySelector(".assunto h1")
+
+    divIcone.classList.add(assunto.toLowerCase())
+    iconeImg.setAttribute("src", `../../assets/images/icon-${assunto.toLowerCase()}.svg`)
+    iconeImg.setAttribute("alt", `icone de ${assunto}`)
+    assuntoTitulo.innerText = assunto
+}
+
+alterarAssunto()
